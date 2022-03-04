aa307a1d1d84ce48545e8126f41078cdc3773ecb
Steven1991 <nguenkochimi@gmail.com>
2022-03-04 05:53:07 +0100

    first commit of restoders

---
+++ src/test/java/com/orders/restOrders/RestOrdersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orders.restOrders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestOrdersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
